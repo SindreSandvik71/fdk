50420fb2d58eea6771e078d8f727591dfd5bc59c
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-05-29 09:24:29 +0200

    update relative path

--- applications/search/test/components/search-app-breadcrumbs/index.test.jsx
+++ applications/search/test/components/search-app-breadcrumbs/index.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Breadcrumbs } from '../../../../search/src/components/search-app-breadcrumbs';
+import { Breadcrumbs } from '../../../src/components/search-app-breadcrumbs';
 
 test('should render Breadcrumbs correctly', () => {
   const wrapper = shallow(<Breadcrumbs />);
