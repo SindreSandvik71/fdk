7320e6c35be3d6bcea98b5da739e06f920b56c7c
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-06-21 08:25:18 +0200

    (registration-react) FormTitle registration with language

--- applications/registration-react/src/components/reg-form-schema-title/formValidations.js
+++ applications/registration-react/src/components/reg-form-schema-title/formValidations.js
@@ -3,14 +3,22 @@ import {
   validateMinTwoChars,
   validateURL
 } from '../../validation/validation';
+import localization from '../../utils/localization';
 
 const validate = values => {
   let errors = {};
-  const title = values.title && values.title.nb ? values.title.nb : null;
+  const title =
+    values.title && values.title[localization.getLanguage()]
+      ? values.title[localization.getLanguage()]
+      : null;
   const description =
-    values.description && values.description.nb ? values.description.nb : null;
+    values.description && values.description[localization.getLanguage()]
+      ? values.description[localization.getLanguage()]
+      : null;
   const objective =
-    values.objective && values.objective.nb ? values.objective.nb : null;
+    values.objective && values.objective[localization.getLanguage()]
+      ? values.objective[localization.getLanguage()]
+      : null;
   const landingPage =
     values.landingPage && values.landingPage[0] ? values.landingPage[0] : null;
 
